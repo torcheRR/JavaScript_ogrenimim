1ecc3dab920b5ab0dbbf68d56c1d8015daa4436d
torcheRR <berkayocer2003@gmail.com>
2023-07-20 10:25:34 +0300

    v

--- 023/023-functions-bolum-sonu-egzersizleri.js
+++ 023/023-functions-bolum-sonu-egzersizleri.js
@@ -1,25 +1,33 @@
 // functions bolum sonu egzersizleri
 
-let counter = 0
+let counter = Number(0)
 let counterDOM = document.querySelector(`#counter`)
 let increaseDOM = document.querySelector(`#increase`)
 let decreaseDOM = document.querySelector(`#decrease`)
 
 counterDOM.innerHTML = counter
 
-increaseDOM.addEventListener("click", clickevent)
-decreaseDOM.addEventListener("click", clickevent)
+increaseDOM.addEventListener("click" , clickEvent)
+decreaseDOM.addEventListener("click" , clickEvent)
 
-function clickevent(){
+// function clickEvent(){
+//     console.log(this.id)
+//     if(this.id=="increase"){
+//         counterDOM.innerHTML ++
+//     }
+//     else {
+//         counterDOM.innerHTML --
+//     }
+// }
+
+
+// function clickEvent(){
+//     console.log(this.id)
+//     this.id=="increase" ? counterDOM.innerHTML = counter +=1 : counterDOM.innerHTML = counter -=1
+// }
+
+function clickEvent(){
     console.log(this.id)
-    if(this.id=="increase"){
-        counterDOM.innerHTML = counter += 1
-    }
-    else{
-        counterDOM.innerHTML = counter -= 1
-    }
-
-
-    // this.id == "increase" ? counter +=1 : counter -=1
-    // counterDOM.innerHTML=counter
+    this.id=="increase" ? counter +=1 : counter -=1
+    counterDOM.innerHTML = counter
 }
